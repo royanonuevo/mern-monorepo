cf66fc78b82f06db9f26fe2e5e03ee29877b9258
Roy Anonuevo <roy.anonuevo@growthops.asia>
2022-11-12 11:08:06 +0800

    monorepo yarn workspace

--- packages/client/pages/dashboard.tsx
+++ packages/client/pages/dashboard.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import Sidebar from '@ui/layouts/Sidebar'
 import PageHead from '@ui/layouts/PageHead'
+import Test from '@common/Test'
 
 const Dashboard: NextPage = () => {
   return (
     <>
       <PageHead />
       <main>
+        <Test />
         <Sidebar />
       </main>
     </>
---
+++ packages/ui/Test.jsx
@@ -0,0 +1,9 @@
+const Test = () => {
+  return (
+    <div className="text-blue-400">
+      test component
+    </div>
+  )
+}
+
+export default Test
